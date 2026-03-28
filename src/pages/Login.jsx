7365865f8d26d5aa76774efbfc01daba350d684c
sarvani <2500030027@kluniversity.com>
2026-03-28 12:07:11 +0530

    change

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import "./account.css";   // ✅ VERY IMPORTANT
 
 export default function Login() {
 
@@ -13,46 +14,37 @@ export default function Login() {
     const savedUser = JSON.parse(localStorage.getItem("user"));
 
     if(savedUser && savedUser.email === email && savedUser.password === password){
-
       alert("Login Successful");
-
-      navigate("/dashboard");   // 🔹 This sends user to dashboard
-
+      navigate("/dashboard");
     } else {
-
       alert("Invalid Email or Password");
-
     }
 
   };
 
   return (
+    <div className="account-page">
+      <div className="profile-card">
 
-    <div>
+        <h2>Login</h2>
 
-      <h2>Login</h2>
+        <input
+          type="email"
+          placeholder="Enter Email"
+          onChange={(e)=>setEmail(e.target.value)}
+        />
 
-      <input
-      type="email"
-      placeholder="Enter Email"
-      onChange={(e)=>setEmail(e.target.value)}
-      />
+        <input
+          type="password"
+          placeholder="Enter Password"
+          onChange={(e)=>setPassword(e.target.value)}
+        />
 
-      <br/><br/>
+        <button onClick={handleLogin}>
+          Login
+        </button>
 
-      <input
-      type="password"
-      placeholder="Enter Password"
-      onChange={(e)=>setPassword(e.target.value)}
-      />
-
-      <br/><br/>
-
-      <button onClick={handleLogin}>
-        Login
-      </button>
-
+      </div>
     </div>
-
   );
 }
